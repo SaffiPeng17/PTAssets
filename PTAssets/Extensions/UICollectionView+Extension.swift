bede6e532fe6e84b1bf92c7389d01caf5411cfff
Saffi P <greenb17@gmail.com>
2023-02-06 17:18:22 +0800

    ✨ Add UICollectionView extension

---
+++ PTAssets/Extensions/UICollectionView+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UICollectionView+exteions.swift
+//  PTAssets
+//
+//  Created by Saffi on 2023/2/6.
+//
+
+import UIKit
+
+extension UICollectionView {
+
+    func dequeueReusableCell<T: UICollectionViewCell>(_ type: T.Type,
+                                                      identifier: String = String(describing: T.self),
+                                                      for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as? T else {
+            fatalError("dequeue cell failed with identifier: \(identifier)")
+        }
+        return cell
+    }
+}
