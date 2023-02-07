611d01fb8e52bccc89e7648a83d8a19782b56a74
Saffi P <greenb17@gmail.com>
2023-02-07 14:28:50 +0800

    ✨ Add register & dequeue methods for header/footer into UICollectionView extension

--- PTAssets/Extensions/UICollectionView+Extension.swift
+++ PTAssets/Extensions/UICollectionView+Extension.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
+// MARK: - Register/Dequeue customized cell
 extension UICollectionView {
+
     func register<T: UICollectionViewCell>(_ type: T.Type,
                                            identifier: String = String(describing: T.self)) {
         register(T.self, forCellWithReuseIdentifier: identifier)
@@ -20,5 +22,38 @@ extension UICollectionView {
             fatalError("dequeue cell failed with identifier: \(identifier)")
         }
         return cell
+    }
+}
+
+// MARK: - Register/Dequeue customized resuable header and footer
+extension UICollectionView {
+
+    enum ReusableViewType {
+        case header, footer
+
+        var kind: String {
+            switch self {
+            case .header: return UICollectionView.elementKindSectionHeader
+            case .footer: return UICollectionView.elementKindSectionFooter
+            }
+        }
+    }
+    
+    func register<T: UICollectionReusableView>(_ type: T.Type,
+                                               reusableViewType resuableType: ReusableViewType,
+                                               identifier: String = String(describing: T.self)) {
+        register(T.self, forSupplementaryViewOfKind: resuableType.kind, withReuseIdentifier: identifier)
+    }
+
+    func dequeueReusableView<T: UICollectionReusableView>(_ type: T.Type,
+                                                          reusableViewType resuableType: ReusableViewType,
+                                                          identifier: String = String(describing: T.self),
+                                                          indexPath: IndexPath) -> T {
+        guard let reusableView = dequeueReusableSupplementaryView(ofKind: resuableType.kind,
+                                                                  withReuseIdentifier: identifier,
+                                                                  for: indexPath) as? T else {
+            fatalError("dequeue reusable view failed with identifier: \(identifier)")
+        }
+        return reusableView
     }
 }
